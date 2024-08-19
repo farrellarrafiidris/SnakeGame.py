6a45db13d399678a6195e5581487399520493398
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-08-19 10:33:05 +0700

    Modified

--- app.py
+++ app.py
@@ -49,17 +49,45 @@ def next_turn(snake, food):
     square = canvas.create_rectangle(x, y, x + SPACE_SIZE , y + SPACE_SIZE, fill= SNAKE_COLOR)
     snake.squares.insert(0, square)
 
-    del snake.coordinates[-1]
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+        global score 
+        score += 1
 
-    canvas.delete(snake.squares[-1])
+        label.config(text="Score:{}".format(score))
 
-    del snake.squares[-1]
+        canvas.delete(f"food")
+
+        food = Food()
+    
+    else:
+
+        del snake.coordinates[-1]
+
+        canvas.delete(snake.squares[-1])
+
+        del snake.squares[-1]
 
     window.after(GAME_SPEED , next_turn, snake, food)
 
 
 def change_direction(new_direction):
-    pass
+    global direction
+
+    if new_direction == 'left':
+        if direction!= 'right':
+            direction = new_direction
+
+    elif new_direction == 'right':
+        if direction!= 'left':
+            direction = new_direction
+
+    elif new_direction == 'up':
+        if direction!= 'down':
+            direction = new_direction
+
+    elif new_direction == 'down':
+        if direction!= 'up':
+            direction = new_direction
 
 def check_collision():
     pass
@@ -92,6 +120,11 @@ x = int((screen_width / 2) - (window_width / 2))
 y = int((screen_height / 2) - (window_height / 2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
 
 snake = Snake()
 food = Food()
